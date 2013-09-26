826cbec4f4ad33cc7ebf37fd2cba3b5f59c4db42
djkn0x <dan@scienceexchange.com>
2013-09-26 23:52:51 +0000

    Stock scraper from Tuesday's class

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,2 +1,45 @@
 import json
+import urllib
+import os
+import time
 
+dir_out = '/home/GA10/data/yahoo_finance_data'
+if not os.path.exists(dir_out):
+    os.mkdir(dir_out)
+    print 'created directory %s' % dir_out
+
+threshold = 60
+symbols = ['SAP']
+tmp = []
+
+for symbol in symbols:
+    time.sleep(.5)
+    data_out = {}
+    threshold_l = 'less than %s' % threshold
+    threshold_h = 'more than %s' % threshold
+    try:
+        if threshold_l not in data_out:
+            data_out[threshold_l]=0
+        if threshold_h not in data_out: 
+            data_out[threshold_h]=0
+        url = 'http://ichart.finance.yahoo.com/table.csv?s=%s&d=6&e=24&f=2013&g=d&a=4&b=18&c=2012&ignore=.csv' % (symbol)
+        data = urllib.urlopen(url).read()
+        print data
+        file = '%s/%s_data.csv' % (dir_out, symbol)
+        print 'savingfile %s' % file
+        f = open(file, 'w')
+        f.write('%s' % str(data))
+        f.close()
+        data = data.split('\n')
+        for d in data[1:-1]:
+            try: 
+                d = d.split(',')
+                close = d[6]
+                tmp.append(close)
+                if float(close)>=float(threshold):
+                    data_out[threshold_h]+=1
+                elif float(close)<float(threshold):
+                    data_out[threshold_l]+=1
+            except:pass
+    except:pass
+    print json.dumps(data_out)
