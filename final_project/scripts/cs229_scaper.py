a55ca43f88c6f2ff7ad72c828647ed86359767f8
Dan Knox <dan@scienceexchange.com>
2013-10-29 23:05:20 -0700

    Updates scraper with full plan

--- final_project/scripts/cs229_scaper.py
+++ final_project/scripts/cs229_scaper.py
@@ -4,8 +4,13 @@ from time import sleep
 import csv
 
 for a in range(2005,2013):
+    # create a directory "data/a"
     url = "http://cs229.stanford.edu/projects" + str(a) + ".html"
+    # visit page
+    # download each PDF on page 
+    # save each PDF in directory
     print url
+
 
 
 soup = bs(urlopen(url).read())
