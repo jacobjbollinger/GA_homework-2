754dde7c6bc8e55e3551918d2f715b1bf24b1e19
Dan Knox <dan@scienceexchange.com>
2013-11-13 20:41:10 -0800

    Initial analysis of CS 229 data

---
+++ final_project/scripts/tokenize_and_bigrams.py
@@ -0,0 +1,50 @@
+# Must be run from root (i.e. final_project) folder
+
+from time import sleep
+import os
+import re
+import collections
+
+from nltk.corpus import stopwords
+
+from nltk.tokenize import regexp_tokenize
+
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+from nltk.collocations import TrigramCollocationFinder
+from nltk.metrics import TrigramAssocMeasures
+
+punctuation = re.compile(r'[-.?!%,";:()|0-9]')
+
+for a in range(2012,2014):
+
+	sleep(1)
+	print "\n...starting %s files" % str(a)
+
+	directory = "data/text/%s" % str(a)
+	files = os.listdir(directory)
+
+	for f in files:
+
+		if ".txt" in f:
+			try:
+
+				filename = directory + "/" + f
+				
+				# === Word list and stopwords ===
+
+				word_list = re.split('\s+', open(filename).read().lower())
+				words = (punctuation.sub("", word).strip() for word in word_list)
+				words = (word for word in words if word not in stopwords.words('english'))
+
+				# === Ngrams ===
+				
+				bcf = BigramCollocationFinder.from_words(words)
+				print bcf.nbest(BigramAssocMeasures.likelihood_ratio, 10)
+
+				tcf = TrigramCollocationFinder.from_words(words)
+				print tcf.nbest(TrigramAssocMeasures.likelihood_ratio, 10)
+				
+			except: 
+				pass
+
